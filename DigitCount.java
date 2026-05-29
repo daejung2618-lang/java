23782b8b817f81c9e67a1697b098298ad0de2ce4
daejung2618-lang <daejung2618@dge.go.kr>
2026-05-29 10:12:45 +0900

    Add files via upload

---
+++ DigitCount.java
@@ -0,0 +1,10 @@
+package jumptojava;
+
+public class DigitCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SquareStar.java
@@ -0,0 +1,10 @@
+package jumptojava;
+
+public class SquareStar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
